a1ff1059a3b3025e6ab41f59815963b24a7f788f
skellz <sherwin.kelly@gmail.com>
2013-11-23 17:03:19 -0500

    The program now allows the user to enter multiple lines of input when they enter "I have a lot to say" afte the 1st prompt It returns multiple lines in the format of: "You said:"... "Then, you said:"... "Then, you said:"... "Finally you said:"... "Phew! Glad you got all" number_of_statements "of those things off your chest!"

--- echo.rb
+++ echo.rb
@@ -7,9 +7,28 @@ def playback(input)
   if input == "Nothing!"
     puts  "Ok fine!"
   elsif input == "I have a lot to say"
-    puts "I don't have time for that right now!"
+    input_array =[]
+    until input == "done"
+      input = gets.chomp.to_s
+      input_array << input
+    end
+    input_array.pop
+    input_amt = input_array.length
+    if input_array.length > 2
+      puts "You said: #{input_array[0]}"
+      input_array.shift
+      last = input_array.pop      
+      input_array.each do |x|
+        puts "Then, you said: #{x}"
+      end
+      puts "Finally you said: #{last}"
+    else
+      puts "You said: #{input_array[0]}"
+      puts "Finally you said: #{input_array[1]}"
+    end
+    puts "Phew! Glad you got all #{input_amt} of those things off your chest!"
   else    
-  puts "You said: #{input}"
+    puts "You said: #{input}"
   end
 end
 
